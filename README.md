ae31b98d0a41dc5927cbaa3eee75383c4aec5f0f
nezqt3 <dalekseenko8@gmail.com>
2024-09-27 18:25:31 +0300

    third lesson

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Angular Project with Glo Academy
+
+(┬┬﹏┬┬)
---
+++ films.js
@@ -0,0 +1,29 @@
+export default films = [
+    {
+        id: 0,
+        title: "Марсианин 1",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: "/film.html",
+        image: "./images/Картинка фильма.png"
+    },
+    {
+        id: 1,
+        title: "Марсианин 2",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: "/film.html",
+        image: "./images/Картинка фильма.png"
+    },
+    {
+        id: 2,
+        title: "Марсианин 3",
+        original: "The Martian 2015",
+        category: "Фантастика, приключения",
+        rating: 7.8,
+        link: "/film.html",
+        image: "./images/Картинка фильма.png"
+    }
+]
